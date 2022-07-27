cca41bd78cfce2178b3a6068ff13bcd155b62210
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-28 01:39:36 +0600

    sd

--- tutorial 80, 81  promise  part 1 and part 2/test3.js
+++ tutorial 80, 81  promise  part 1 and part 2/test3.js
@@ -1,9 +1,18 @@
 console.log("welcome");
 
 
-const promise = new Promise((resolve, reject) =>{
+const promise1 = new Promise((resolve, reject) =>{
+
+  let completedPromise = true;
+  if (completedPromise ){
+    resolve("completed Promise 1");
+  }
+  else{
+    reject("Not promise 1 completed")
+  }
 
 });
 
+promise1.then()
 
 console.log("end")
