27a3c3a252a54fa909e939387c1f83e1a964d65c
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-28 23:40:30 +0600

    change

--- tutorial 80, 81  promise  part 1 and part 2/test3.js
+++ tutorial 80, 81  promise  part 1 and part 2/test3.js
@@ -2,26 +2,40 @@
 console.log("welcome");
 
 const promise1 = new Promise((resolve, reject) => {
-  let completedPromise = false;
+  let completedPromise = true;
   if (completedPromise) {
-    resolve("completed Promise 1");
+    setTimeout(() => {
+      resolve("completed Promise 1");
+    },2000);
   } else {
     reject(new Error("Not promise 1 completed"));
   }
 });
 
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("completed Promise 2");
+    },1000)
+});
+
+//race jar time kom take niye kaj korbe and take age dekhiye debe output y
+Promise.race([promise1, promise2]).then((res) =>
+  console.log(res)
+);
 
 
+//at the same time we can call the promise
 
+// Promise.all([promise1, promise2]).then(([res1, res2]) =>
+//   console.log(res1, res2)
+// );
 
-
-promise1
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+// promise1
+//   .then((res) => {
+//     console.log(res);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
 
 console.log("end");
-
