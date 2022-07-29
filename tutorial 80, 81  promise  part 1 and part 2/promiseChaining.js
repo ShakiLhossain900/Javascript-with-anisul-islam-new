594027b6163c1bd5f6095f842e9cc78fbb03cf73
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-07-29 18:44:16 +0600

    new

--- tutorial 80, 81  promise  part 1 and part 2/promiseChaining.js
+++ tutorial 80, 81  promise  part 1 and part 2/promiseChaining.js
@@ -1,5 +1,5 @@
 //promise chaining
-//async function 
+//async function
 //Synchronous = happens at the same time. Asynchronous = doesn't happen at the same time
 const taskOne = () => {
   return new Promise((resolve, reject) => {
@@ -8,14 +8,14 @@ const taskOne = () => {
 };
 const taskTwo = () => {
   return new Promise((resolve, reject) => {
-     setTimeout(() =>{
-        resolve("task -> two is completed");
-     },2000)
+    setTimeout(() => {
+      resolve("task -> two is completed");
+    }, 2000);
   });
 };
 const taskThree = () => {
   return new Promise((resolve, reject) => {
-    resolve("task -> Three is completed");
+    reject("task -> Three is not completed");
   });
 };
 const taskFour = () => {
@@ -25,23 +25,26 @@ const taskFour = () => {
 };
 
 //async await
-async function callAllTasks (){
+
+// async function callAllTasks (){
+const callAllTasks = async () => {
   //this async await is mostly work if t1 is completed the it will run for t2
-    const t1 = await taskOne ();
+
+  try {
+    const t1 = await taskOne();
     console.log(t1);
-    const t2 = await taskTwo ();
+    const t2 = await taskTwo();
     console.log(t2);
-    const t3 = await taskThree ();
+    const t3 = await taskThree();
     console.log(t3);
-    const t4 = await taskFour ();
+    const t4 = await taskFour();
     console.log(t4);
-}
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 callAllTasks();
-
-
-
-
 
 //promise chaining er modde call koreci
 // taskOne()
@@ -53,6 +56,3 @@ callAllTasks();
 // .then(taskFour)
 // .then(res => {console.log(res)})
 // .catch((error) => {console.log(error)})
-
-
-
