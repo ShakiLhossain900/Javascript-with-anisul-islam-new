f22074ece19a5759bd56cef413321d374523e6bc
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-23 11:54:05 +0600

    created promise

--- tutorial 82  async await javascript/asyncAwit.js.js
+++ tutorial 82  async await javascript/asyncAwit.js.js
@@ -1,1 +1,27 @@
 // creating the promise for async and await operations
+
+const taskOne = () => {
+    return new Promise((resolve, reject) =>{
+        resolve("task -> one is completed");
+    })
+};
+
+const taskTwo = () => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve("task -> two is completed");
+      }, 2000);
+    });
+  };
+ 
+const taskThree = () => {
+    return new Promise((resolve, reject) => {
+        reject("task -> three is completed");
+    });
+};
+
+const taskFour = () => {
+    return new Promise((resolve, reject) => {
+        resolve("task -> four is completed");
+    })
+}
