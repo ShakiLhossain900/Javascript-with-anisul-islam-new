a3256236d45ae3eec0845fae9fd72a9df14f81c5
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-09-23 22:42:19 +0600

    async await

--- tutorial 82  async await javascript/asyncAwit.js.js
+++ tutorial 82  async await javascript/asyncAwit.js.js
@@ -16,7 +16,7 @@ const taskTwo = () => {
  
 const taskThree = () => {
     return new Promise((resolve, reject) => {
-        reject("task -> three is completed");
+        reject("task -> three is not completed");
     });
 };
 
@@ -25,3 +25,25 @@ const taskFour = () => {
         resolve("task -> four is completed");
     })
 }
+
+// totokkhoon move kortece na jotokkhon tar kaj ses na hocce 
+const callAllTasks = async () => {
+ 
+    try {
+        const t1 = await taskOne();
+        console.log(t1);
+        const t2 = await taskTwo();
+        console.log(t2);
+        const t3 = await taskThree();
+        console.log(t3);
+        const t4 = await taskFour();
+        console.log(t4);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+
+
+
+callAllTasks();
